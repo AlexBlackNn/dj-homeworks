1958ea37db83f1f59f74a144206f1119bdc7c991
Alex_Black <AlexBlackNn@yandex.ru>
2022-02-21 00:04:08 +0300

    сделал вывод учителей для каждого ученика

--- 2.2-databases-2/orm_migrations/school/admin.py
+++ 2.2-databases-2/orm_migrations/school/admin.py
@@ -1,13 +1,29 @@
 from django.contrib import admin
 
-from .models import Student, Teacher
+from .models import Student, Teacher, StudentTeacher
 
+
+class StudentTeacherInline(admin.TabularInline):
+    model = StudentTeacher
+    extra = 1
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    pass
-
+    list_display = ('pk', 'name', 'group',)
+    list_editable = ('group', )
+    search_fields = ('group',)
+    list_filter = ('teacher',)
+    empty_value_display = '-пусто-'
+    inlines = [StudentTeacherInline]
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('pk', 'name', 'subject',)
+    list_editable = ('subject',)
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+
+
--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -73,8 +73,10 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_orm_migrations',
+        'NAME': 'netology_orm_relations',
         'HOST': '127.0.0.1',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
         'PORT': '5432',
     }
 }
@@ -102,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'RU-ru'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'Europe/Moscow'
 
