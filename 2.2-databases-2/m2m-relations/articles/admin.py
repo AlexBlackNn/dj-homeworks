ad98c7a97b0ed8ced7e21a0bd8012426b21cf200
alexblacknn <alexblacknn@yandex.ru>
2022-03-15 18:44:54 +0300

    something even works

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Scope
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['pk', 'title', 'text', 'published_at', 'image']
+
+
+@admin.register(Scope)
+class ScopeAdmin(admin.ModelAdmin):
+    list_display = ['is_main', 'tag']
